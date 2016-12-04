510c69275542574c05fe5842e250b3776fd54407
Mikita Seradzinski <nikseredinskiy@gmail.com>
2016-12-04 21:14:13 +0300

    Added menu

--- main.py
+++ main.py
@@ -4,19 +4,32 @@ import sqlite3 as lite
 
 class Account(object):
     def __init__(self, holder_name):
-        self.id = random.randint(0, 99999)
+        self.id = random.randint(0, 999)
         self.holder_name = holder_name
         self.amount = 0
-        accounts[self.id] = self
-        self.save_account_to_database()
+
+    @staticmethod
+    def create_account(holder_name):
+        temp = Account(holder_name)
+        Account.save_account_to_database(temp)
+        print("Account successfully created")
+        return temp.id
+
+    @staticmethod
+    def init_account(acc_id, holder_name, amount):
+        temp = Account(holder_name)
+        temp.id = acc_id
+        temp.amount = amount
+        return temp
 
     def save_account_to_database(self):
         con = lite.connect('database.db')
         with con:
             cur = con.cursor()
-            cur.execute("""INSERT INTO accounts(id, holder_name, amount) VALUES (?, ?, ?);""", (self.id,
-                                                                                                self.holder_name,
-                                                                                                self.amount))
+            cur.execute("""INSERT INTO accounts (id, holder_name, amount)
+                            SELECT * FROM (SELECT ?, ?, ?) AS tmp
+                            WHERE NOT EXISTS (
+                            SELECT id FROM accounts WHERE id = ?) LIMIT 1;""", (self.id, self.holder_name, self.amount, self.id))
             con.commit()
 
     @staticmethod
@@ -28,13 +41,20 @@ class Account(object):
             con.commit()
 
     @staticmethod
-    def get_all_accounts():
+    def fetch_accounts():
         con = lite.connect('database.db')
         with con:
             cur = con.cursor()
-            cur.execute("SELECT (id, holder_name, amount) FROM accounts")
+            cur.execute("SELECT * FROM accounts")
 
             return cur.fetchall()
+
+    @staticmethod
+    def get_all_accounts():
+        temp = dict()
+        for acc in Account.fetch_accounts():
+            temp[acc[0]] = Account.init_account(acc[0], acc[1], acc[2])
+        return temp
 
     @staticmethod
     def make_transaction(transaction):
@@ -58,11 +78,13 @@ class Account(object):
             print("Source account doesn't exist")
             print("Transaction doesn't complete")
 
-    def increase_amount(self, amount):
+    @staticmethod
+    def increase_amount(account_id, amount):
         if amount > 0:
-            source_account = accounts.get(self.id)
+            source_account = accounts.get(account_id)
             source_account.amount += amount
             Account.update_account_amount(source_account)
+            print("Amount successfully increased")
 
     def print_account_info(self):
         print("{0}'s account {1} info: Amount = {2}".format(self.holder_name, self.id, self.amount))
@@ -75,14 +97,71 @@ class Transaction(object):
         self.final_account_id = final_account_id
         self.amount = amount
 
+
+class Menu(object):
+    @staticmethod
+    def print_header():
+        print(""" -------------------------------------""")
+        print("""|          Belnikserbank              |""")
+        print(""" -------------------------------------""")
+
+    @staticmethod
+    def print_main_operations_list():
+        print("""|1. Create new account                |""")
+        print("""|2. Increase amount                   |""")
+        print("""|3. Show accounts list                |""")
+        print("""|4. Print account info                |""")
+        print("""|5. Make transaction                  |""")
+        print("""|6. Cancel transaction                |""")
+        print("""|7. Delete this account               |""")
+        print("""|8. Quit application                  |""")
+
+    @staticmethod
+    def print_footer():
+        print(""" -------------------------------------""")
+
+    @staticmethod
+    def execute_operating(operation_number, current_account_id):
+        if operation_number == "1":
+            holder_name = input("Please enter HOLDER_NAME of the new account: ")
+            Account.create_account(holder_name)
+        if operation_number == "2":
+            amount = input("Enter sum of increasing: ")
+            Account.increase_amount(current_account_id, int(amount))
+        if operation_number == "3":
+            temp = Account.get_all_accounts()
+            Menu.print_header()
+            for i in temp:
+                print("""|ID:{0} Name:{1}""".format(temp[i].id, temp[i].holder_name) + " " * (37 - 9 - len(str(temp[i].id) + temp[i].holder_name)) + "|")
+            Menu.print_footer()
+        if operation_number == "4":
+            temp = accounts[current_account_id]
+            Menu.print_header()
+            print("|" + str(temp.id) + " " + temp.holder_name + " " + str(temp.amount) + " " * (37 - 2 - len(str(temp.id) + temp.holder_name + str(temp.amount))) + "|")
+            Menu.print_footer()
+        if operation_number == "5":
+            final_account_id = input("Enter destination ID: ")
+            amount = input("Enter amount of the transaction: ")
+            Account.make_transaction(Transaction(current_account_id, int(final_account_id), int(amount)))
+
+
 accounts = Account.get_all_accounts()
-first_account = Account("Mikita Seradzinski")
-first_account.increase_amount(1000000)
-second_account = Account("Anastasia Sadovaya")
-
-first_account.print_account_info()
-second_account.print_account_info()
-
-Account.make_transaction(Transaction(first_account.id, second_account.id, 1000))
-first_account.print_account_info()
-second_account.print_account_info()
+login_id = input("Please enter your account ID: ")
+is_working = True
+while is_working:
+    if accounts.get(int(login_id), "404") != "404":
+        Menu.print_header()
+        Menu.print_main_operations_list()
+        Menu.print_footer()
+        operation = input("Enter number of the operation: ")
+        if operation != "8":
+            Menu.execute_operating(operation, int(login_id))
+            accounts = Account.get_all_accounts()
+        else:
+            is_working = False
+    else:
+        print("No such account")
+        name = input("Enter your name for the new account: ")
+        login_id = Account.create_account(name)
+        accounts = Account.get_all_accounts()
+print("Bye. See you soon!")
